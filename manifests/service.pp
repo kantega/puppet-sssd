7bfc455d7e4ff2b73812cc9e29cec51109e7d19f
Tufte Geir Andre <geir.andre.tufte@nte.no>
2021-02-11 15:02:40 +0100

    syntax

--- manifests/service.pp
+++ manifests/service.pp
@@ -4,7 +4,7 @@ class sssd::service inherits ::sssd::params {
     ensure => $::sssd::params::sssd_service_ensure,
     enable => $::sssd::params::sssd_service_enable,
   }
-  exec { 'Restart SSSD service if backend is offline';
+  exec { 'Restart SSSD service if backend is offline':
     command => '/bin/systemctl restart sssd',
     user => 'root',
     onlyif => '/bin/tail -n 1 /var/log/sssd/sssd_nss.log | /bin/grep \'Error.DataProvider.Offline\.',
